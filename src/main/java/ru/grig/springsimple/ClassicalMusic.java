8867681ced345142f6827891bbd9b983c87bccd6
Aleksei Grigorev <akgrig_work@mail.ru>
2023-07-21 13:10:05 +0300

    commit from notebook with scope

--- src/main/java/ru/grig/springsimple/ClassicalMusic.java
+++ src/main/java/ru/grig/springsimple/ClassicalMusic.java
@@ -1,9 +1,15 @@
 package ru.grig.springsimple;
 
 public class ClassicalMusic implements Music{
-
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+    public void doMyDestroy(){
+        System.out.println("Doing my destroy");
+    }
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
     }
+
 }
--- src/main/java/ru/grig/springsimple/TestSpring.java
+++ src/main/java/ru/grig/springsimple/TestSpring.java
@@ -7,15 +7,16 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-
-        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
-        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
-
-        boolean comparison = musicPlayer1==musicPlayer2;
-
-        System.out.println(comparison);
-        System.out.println(musicPlayer1);
-        System.out.println(musicPlayer2);
+        ClassicalMusic classicalMusic = context.getBean("musicBean", ClassicalMusic.class);
+        System.out.println(classicalMusic.getSong());
+//        MusicPlayer musicPlayer1 = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = musicPlayer1==musicPlayer2;
+//
+//        System.out.println(comparison);
+//        System.out.println(musicPlayer1);
+//        System.out.println(musicPlayer2);
 
         context.close();
     }
