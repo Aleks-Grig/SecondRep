6205cb8450cfe69bce9a8dc47611a3695405b489
Pyro <akgrig_work@mail.ru>
2023-07-21 20:40:10 +0300

    first program with using annotations in spring

--- src/main/java/ru/grig/springsimple/ClassicalMusic.java
+++ src/main/java/ru/grig/springsimple/ClassicalMusic.java
@@ -1,20 +1,9 @@
 package ru.grig.springsimple;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClassicalMusic implements Music {
-    private ClassicalMusic() {}
-
-    public static ClassicalMusic getClassicalMusic(){
-        return new ClassicalMusic();
-    }
-
-    public void doMyInit() {
-        System.out.println("Doing my initialization");
-    }
-
-    public void doMyDestroy() {
-        System.out.println("Doing my destroy");
-    }
-
     @Override
     public String getSong() {
         return "Hungarian Rhapsody";
--- src/main/java/ru/grig/springsimple/TestSpring.java
+++ src/main/java/ru/grig/springsimple/TestSpring.java
@@ -7,13 +7,13 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        ClassicalMusic classicalMusic1 = context.getBean("musicBean", ClassicalMusic.class);
-        ClassicalMusic classicalMusic2 = context.getBean("musicBean", ClassicalMusic.class);
+        Music music1 = context.getBean("rockMusic", Music.class);
+        Music music2 = context.getBean("classicalMusic", Music.class);
+        MusicPlayer musicPlayer1 = new MusicPlayer(music1);
+        MusicPlayer musicPlayer2 = new MusicPlayer(music2);
+        musicPlayer1.playMusic();
+        musicPlayer2.playMusic();
 
-        boolean comparison = classicalMusic1==classicalMusic2;
-
-        System.out.println(classicalMusic1.getSong());
-        System.out.println(comparison);
         context.close();
     }
 }
